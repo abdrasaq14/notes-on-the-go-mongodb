3f44239dfc6c320ddbe432e25433e4900d37a22f
abdrasaq14 <abdrasaq14@gmail.com>
2024-03-22 20:47:20 +0100

    adjusted some styling

--- schema/noteDB.ts
+++ schema/noteDB.ts
@@ -16,26 +16,26 @@ export interface userInterface extends Document {
   phone_no: string;
 }
 const userSchema = new mongoose.Schema({
-  fullname: { type: String, required: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
-  authentication: {
-    password: { type: String, required: true, select: false },
-    salt: { type: String, select: false },
-    sessionToken: { type: String, select: false },
-  },
+	fullname: { type: String, required: true },
+	email: { type: String, required: true, unique: true, lowercase: true },
+	authentication: {
+		password: { type: String, required: true, select: false },
+		salt: { type: String, select: false },
+		sessionToken: { type: String, select: false },
+	},
 
-  phone_no: { type: String },
+	phone_no: { type: String },
 });
 
 const notesSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  due_date: { type: String, required: true },
-  status: { type: String, required: true },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "UserModel",
-  },
+	title: { type: String, required: true },
+	description: { type: String, required: true },
+	due_date: { type: String, required: true },
+	status: { type: String, required: true },
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "UserModel",
+	},
 });
 
 export const UserModel = mongoose.model("UserModel", userSchema);
@@ -45,43 +45,43 @@ export const NotesModel = mongoose.model("NotesModel", notesSchema);
 export const getUsers = () => UserModel.find();
 export const getUserByEmail = (email: string) => UserModel.findOne({ email });
 export const getUserBySessionToken = (sessionToken: string) =>
-  UserModel.findOne({ "authentication.sessionToken": sessionToken });
+	UserModel.findOne({ "authentication.sessionToken": sessionToken });
 export const getUserById = (id: string) => UserModel.findById({ _id: id });
-export const createUser = (values: Record<string, any>) =>
-  new UserModel(values).save().then((userResponse) => userResponse.toObject());
+export const createUser = (values: Record<string, unknown>) =>
+	new UserModel(values).save().then((userResponse) => userResponse.toObject());
 export const deleteUserById = (id: string) =>
-  UserModel.findByIdAndDelete({ _id: id });
+	UserModel.findByIdAndDelete({ _id: id });
 
-export const updateUserById = (id: string, values: Record<string, any>) =>
-  UserModel.findByIdAndUpdate(id, values);
+export const updateUserById = (id: string, values: Record<string, unknown>) =>
+	UserModel.findByIdAndUpdate(id, values);
 
 //notes methods
 export const getNotes = () => NotesModel.find();
 export const getNotesById = (userId: string) =>
-  NotesModel.find({ author: userId });
+	NotesModel.find({ author: userId });
 export const getNotesByAuthorId = (userId: string) =>
-  NotesModel.find({ author: userId });
-export const createNote = (values: Record<string, any>) => {
-  new NotesModel(values)
-    .save()
-    .then((notesResponse) => notesResponse.toObject());
+	NotesModel.find({ author: userId });
+export const createNote = (values: Record<string, unknown>) => {
+	new NotesModel(values)
+		.save()
+		.then((notesResponse) => notesResponse.toObject());
 };
 export const deleteNotesById = (id: string) =>
-  NotesModel.findByIdAndDelete({ _id: id });
+	NotesModel.findByIdAndDelete({ _id: id });
 
 export const updateNotesById = async (
-  id: string,
-  values: Record<string, any>
+	id: string,
+	values: Record<string, unknown>
 ) => {
-  try {
-    const updatedNotes = await NotesModel.findByIdAndUpdate(id, values, {
-      new: true,
-    });
-    return updatedNotes;
-  } catch (error) {
-    console.error("Error updating notes:", error);
-    throw error; // Rethrow the error to be caught by the calling code if needed
-  }
+	try {
+		const updatedNotes = await NotesModel.findByIdAndUpdate(id, values, {
+			new: true,
+		});
+		return updatedNotes;
+	} catch (error) {
+		console.error("Error updating notes:", error);
+		throw error; // Rethrow the error to be caught by the calling code if needed
+	}
 };
 
 // user methods
